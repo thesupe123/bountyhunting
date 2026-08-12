cff7e25977b5672bb57451e8958623df949ad1a8
thesupe123 <crusherhack210buisness@gmail.com>
2026-08-12 19:52:57 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,7 +19,7 @@ local jobid = game.JobId
 
 local spawnTP = {
     Vector3.new(1725, 23 -4006),
-    Vector3.new(-1263, 18, -1534),
+    Vector3.new(-1262, 18, -1542),
     Vector3.new(1815, 25, -884),
     Vector3.new(714, 44, 1062),
     Vector3.new(1409, 23, -3914)
