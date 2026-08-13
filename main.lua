b1904cc70f027270720de6970e729937c72666fe
thesupe123 <crusherhack210buisness@gmail.com>
2026-08-13 01:13:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -428,7 +428,7 @@ runService.RenderStepped:Connect(function(dt)
                     targetPlayer = Players:FindFirstChild(topBounty.Username)
                     if targetPlayer then
                         print("Targeting player:", targetPlayer.Name, "with bounty:", topBounty.Bounty)
-                        if targetPlayer.Character and targetPlayer.Character:FindFirstChild("HumanoidRootPart") and targetVehicle then
+                        if targetPlayer.Character and targetPlayer.Character:FindFirstChild("HumanoidRootPart") then
                             if targetVehicle.PrimaryPart.Position.Y < cruisealt then
                                 targetVehicle.PrimaryPart.CFrame = CFrame.new(targetVehicle.PrimaryPart.Position+Vector3.new(0,1,0)*carflyspeed*dt)
                             else
